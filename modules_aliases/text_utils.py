be7f491014816c3ddd549da613449e82a5509a6b
Ryan Boll <bollr38@hotmail.com>
2024-11-06 20:44:09 -0600

    first commit

---
+++ modules_aliases/text_utils.py
@@ -0,0 +1,5 @@
+def reverse_string(phrase):
+    print(phrase[::-1])
+
+def capitalize_string(phrase):
+    print(phrase.upper())
---
+++ mood_module/mood_responses.py
@@ -0,0 +1,11 @@
+def mood_response(mood):
+    if mood == 'happy':
+        print("That's great! Whatever you're doing keep it up.")
+    elif mood == 'sad':
+        print("I hope you find something to brighten your day.")
+    elif mood == 'frustrated':
+        print("For your own mental health do something that makes you feel like you've made some progress.")
+    elif mood == 'ecstatic':
+        print("Write down what you're doing cuz you'll want to repeat it.")
+    else:
+        print("We do not have a response for that but I hope it feels good.")
